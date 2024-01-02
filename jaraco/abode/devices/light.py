71b7097a96e381bcbd0a2df9245786f2f619bfda
darrick <darrick@scollan.org>
2024-01-02 01:30:05 +0000

    Add abode light_bulb type.

--- jaraco/abode/devices/light.py
+++ jaraco/abode/devices/light.py
@@ -22,13 +22,65 @@ class Light(Switch):
 
     tags = ('dimmer', 'dimmer_meter', 'hue')
 
-    def set_color_temp(self, color_temp) -> None:
+    @needs_control_url
+    def set_status(self, status) -> None:
+        if self.type_tag != "device_type.light_bulb":
+            return super().set_status(status)
+
+        """Set device status."""
+        url = urls.INTEGRATIONS + self.uuid
+
+        status = "on" if int(status) == 1 else "off"
+
+        response = self._client.send_request(
+            method="post", path=url, data={"action": status}
+        )
+        response_object = response.json()
+
+        log.debug("Set Status Response: %s", response.text)
+
+        if response_object["idForPanel"] != self.id:
+            raise jaraco.abode.Exception(ERROR.SET_STATUS_DEV_ID)
+
+        if response_object["state"]["powerState"] != str(status).upper():
+            raise jaraco.abode.Exception(ERROR.SET_STATUS_STATE)
+
+        # Note: Status returned is a string of int (e.g. "0") and not
+        # the string indication normally passed for the status (e.g.
+        # "LockClosed"), so can't be used to update self._state.
+
+        log.info("Set device %s status to: %s", self.id, status)
+
+    @needs_control_url
+    def set_level(self, level):
+        """Set device level."""
+        url = urls.INTEGRATIONS + self.uuid
+
+        level_data = {"action": "setpercent", "percentage": int(level)}
+
+        response = self._client.send_request("post", url, data=level_data)
+        response_object = response.json()
+
+        log.debug("Set Level Response: %s", response.text)
+
+        if response_object["idForPanel"] != self.id:
+            raise jaraco.abode.Exception(ERROR.SET_STATUS_DEV_ID)
+
+        if response_object["dimLevel"] != int(level):
+            raise jaraco.abode.Exception(ERROR.SET_STATUS_STATE)
+
+        # self.update(response_object)
+
+        log.info("Set device %s level to: %s", self.id, level)
+
+    @needs_control_url
+    def set_color_temp(self, color_temp):
         """Set device color."""
         url = urls.INTEGRATIONS + self.uuid
 
         color_data = {
-            'action': 'setcolortemperature',
-            'colorTemperature': int(color_temp),
+            "action": "setcolortemperature",
+            "colorTemperature": int(color_temp),
         }
 
         response = self._client.send_request("post", url, data=color_data)
@@ -37,10 +89,10 @@ class Light(Switch):
         log.debug("Set Color Temp URL (post): %s", url)
         log.debug("Set Color Temp Response: %s", response.text)
 
-        if response_object['idForPanel'] != self.id:
+        if response_object["idForPanel"] != self.id:
             raise jaraco.abode.Exception(ERROR.SET_STATUS_DEV_ID)
 
-        if response_object['colorTemperature'] != int(color_temp):
+        if response_object["colorTemperature"] != int(color_temp):
             log.warning(
                 (
                     "Set color temp mismatch for device %s. "
@@ -48,12 +100,12 @@ class Light(Switch):
                 ),
                 self.id,
                 color_temp,
-                response_object['colorTemperature'],
+                response_object["colorTemperature"],
             )
 
-            color_temp = response_object['colorTemperature']
+            color_temp = response_object["colorTemperature"]
 
-        self.update({'statuses': {'color_temp': color_temp}})
+        self.update({"statuses": {"color_temp": color_temp}})
 
         log.info("Set device %s color_temp to: %s", self.id, color_temp)
 
@@ -63,9 +115,9 @@ class Light(Switch):
 
         hue, saturation = color
         color_data = {
-            'action': 'setcolor',
-            'hue': int(hue),
-            'saturation': int(saturation),
+            "action": "setcolor",
+            "hue": int(hue),
+            "saturation": int(saturation),
         }
 
         response = self._client.send_request("post", url, data=color_data)
@@ -74,7 +126,7 @@ class Light(Switch):
         log.debug("Set Color URL (post): %s", url)
         log.debug("Set Color Response: %s", response.text)
 
-        if response_object['idForPanel'] != self.id:
+        if response_object["idForPanel"] != self.id:
             raise jaraco.abode.Exception(ERROR.SET_STATUS_DEV_ID)
 
         # Abode will sometimes return hue value off by 1 (rounding error)
@@ -87,32 +139,32 @@ class Light(Switch):
                 ),
                 self.id,
                 (hue, saturation),
-                (response_object['hue'], response_object['saturation']),
+                (response_object["hue"], response_object["saturation"]),
             )
 
-            hue = response_object['hue']
-            saturation = response_object['saturation']
+            hue = response_object["hue"]
+            saturation = response_object["saturation"]
 
-        self.update({'statuses': {'hue': hue, 'saturation': saturation}})
+        self.update({"statuses": {"hue": hue, "saturation": saturation}})
 
         log.info("Set device %s color to: %s", self.id, (hue, saturation))
 
     @property
     def brightness(self):
         """Get light brightness."""
-        return self.get_value('statuses').get('level')
+        return self.get_value(CONST.STATUSES_KEY).get("level")
 
     @property
     def color_temp(self):
         """Get light color temp."""
-        return self.get_value('statuses').get('color_temp')
+        return self.get_value('statuses').get("color_temp")
 
     @property
     def color(self):
         """Get light color."""
         return (
-            self.get_value('statuses').get('hue'),
-            self.get_value('statuses').get('saturation'),
+            self.get_value(CONST.STATUSES_KEY).get("hue"),
+            self.get_value(CONST.STATUSES_KEY).get("saturation"),
         )
 
     @property
@@ -123,14 +175,18 @@ class Light(Switch):
     @property
     def has_color(self):
         """Device is using color mode."""
-        return self.get_value('statuses').get('color_mode') == str(ColorMode.on)
+        if self.get_value(CONST.STATUSES_KEY).get("color_mode") == str(
+            CONST.COLOR_MODE_ON
+        ):
+            return True
+        return False
 
     @property
     def is_color_capable(self):
         """Device is color compatible."""
-        return 'RGB' in self.type
+        return self.type in ["RGB", "Light Bulb"]
 
     @property
     def is_dimmable(self):
         """Device is dimmable."""
-        return 'Dimmer' in self.type
+        return self.type in ["Dimmer", "Light Bulb"]
